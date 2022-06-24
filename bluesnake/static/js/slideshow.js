678372b2cb421f0f16498fa8fbafa0cf4714fae4
Konstantin <sulwirld@gmail.com>
2022-06-23 21:04:04 -0400

    Fix bugs in slideshow navigation.

--- bluesnake/static/js/slideshow.js
+++ bluesnake/static/js/slideshow.js
@@ -1,10 +1,10 @@
 var slideshows, slides, leftarrow, rightarrow;
-slideshows = document.getElementsByClassName("slideshow");
+slideshows =$(".slideshow");
 
 
 var activated = {};
 var checked = {};
-var allSlidesWereSeen=false;
+var navElements = [];
 
 
 function getPrevSlideIndex(numberOfSlides, slideIndex)
@@ -22,39 +22,50 @@ function getNextSlideIndex(numberOfSlides, slideIndex)
 
 
 function DeactivateSlide(slideshowId, slideList, slideIndex)
-{ 
-    document.getElementById(slideList[slideIndex].id).style.display = "none";
-    document.getElementById(slideList[slideIndex].id + "-navigationbar").style.backgroundColor = "rgb(27, 35, 63)";
-    document.getElementById(slideList[slideIndex].id + "-navigationbar").style.color = "white";
+{  
+    $("#" + slideList[slideIndex].id).css("display", "none");
+    $("#" + slideList[slideIndex].id + "-navigationbar").css("background-color", "rgb(27, 35, 63)");
+    $("#" + slideList[slideIndex].id + "-navigationbar").css("color", "white");
     activated[slideshowId][slideIndex] = false;    
 }
 
 
+function countPercentage(hashmap, slideshowId)
+{
+    var checkedSlidesNumber=0;
+    var SlidesNumber=0;
+    for (var key in hashmap[slideshowId]) {
+        if (hashmap[slideshowId][key]) checkedSlidesNumber++;
+        SlidesNumber++;
+    }
+    return 100 * (checkedSlidesNumber / SlidesNumber);
+}
+
+
 function ActivateSlide(slideshowId, slideList, slideIndex, newAnimation="", activateleftright=true)
-{ 
-    document.getElementById(slideList[slideIndex].id).style.display = "block";  
-    document.getElementById(slideList[slideIndex].id + "-progressbar").style.display = "block";
-    if (newAnimation != "none") document.getElementById(slideList[slideIndex].id).style.animationName = newAnimation;  
-    if (checked[slideshowId] < slideList.length) checked[slideshowId]+=1;
-    document.getElementById(slideshowId).getElementsByClassName("slideshow-percentage")[0].innerHTML = 
-        Math.floor(100*(checked[slideshowId] /  slideList.length)).toString() + "%";  
-    document.getElementById(slideList[slideIndex].id + "-navigationbar").style.backgroundColor = "white";
-    document.getElementById(slideList[slideIndex].id + "-navigationbar").style.color = "rgb(27, 35, 63)";
-    activated[slideshowId][slideIndex] = true;    
+{
+    checked[slideshowId][slideIndex]=true;
+    activated[slideshowId][slideIndex] = true;
+    $("#" + slideList[slideIndex].id).css("display", "block");
+    $("#" + slideList[slideIndex].id+ "-progressbar").css("display", "block"); 
+    if (newAnimation != "") $("#" + slideList[slideIndex].id).css("animation-name", newAnimation);
+    $("#" + slideshowId + " .slideshow-percentage").get(0).innerHTML = Math.floor(countPercentage(checked, slideshowId).toString()) + "%";
+    $("#" + slideList[slideIndex].id + "-navigationbar").css("background-color", "white"); 
+    $("#" + slideList[slideIndex].id + "-navigationbar").css("color", "rgb(27, 35, 63)"); 
 
     var prevslide, nextslide;
     if (slides.length > 0 && activateleftright ) {  
         Left = document.getElementById(slideshowId).getElementsByClassName("left-side")[0];
-        Right = document.getElementById(slideshowId).getElementsByClassName("right-side")[0]; 
+        Right = document.getElementById(slideshowId).getElementsByClassName("right-side")[0];
         prevslide = document.getElementById(slides[getPrevSlideIndex(slides.length, slideIndex)].id).cloneNode(true);
         nextslide = document.getElementById(slides[getNextSlideIndex(slides.length, slideIndex)].id).cloneNode(true);
-        prevslide.style.animationName = newAnimation;  
-        nextslide.style.animationName = newAnimation;  
+        prevslide.style.animationName = newAnimation;
+        nextslide.style.animationName = newAnimation;
         prevslide.id += "-left";nextslide.id += "-right";
         prevslide.style.display = "block";nextslide.style.display = "block";
-        Left.innerHTML= '';Right.innerHTML= ''; 
-        if (slideIndex != 0 || allSlidesWereSeen) Left.appendChild(prevslide);
-        Right.appendChild(nextslide);   
+        Left.innerHTML= '';Right.innerHTML= '';
+        if (slideIndex != 0 || countPercentage(checked, slideshowId) == 100) Left.appendChild(prevslide);
+        Right.appendChild(nextslide);
     }
 }
 
@@ -62,16 +73,16 @@ function ActivateSlide(slideshowId, slideList, slideIndex, newAnimation="", acti
 function ShowPreviousSlide()
 {
     var slideshowId = this.id.toString().split("-")[0] + "-" + this.id.toString().split("-")[1];
-    var slides = document.getElementById(slideshowId).getElementsByClassName("content-zone")[0].getElementsByClassName("slideshow-item"); 
+    var slides = $("#" + slideshowId + " .content-zone .slideshow-item");
 
     var next;
     for (var slideIndex=0;slideIndex<slides.length;slideIndex++)
         if (activated[slideshowId][slideIndex]) {
             next=getPrevSlideIndex(slides.length, slideIndex);break; 
-        }   
-    if (next != slides.length -1 || allSlidesWereSeen) {
-        ActivateSlide(slideshowId, slides, next, "prev-slide"); 
-        DeactivateSlide(slideshowId, slides, getNextSlideIndex(slides.length, next));  
+        }
+    if (!(next == slides.length - 1 && countPercentage(checked, slideshowId) != 100)) {
+        ActivateSlide(slideshowId, slides, next, "prev-slide");
+        DeactivateSlide(slideshowId, slides, getNextSlideIndex(slides.length, next));
     }
 }
 
@@ -79,14 +90,13 @@ function ShowPreviousSlide()
 function ShowNextSlide()
 {
     var slideshowId = this.id.toString().split("-")[0] + "-" + this.id.toString().split("-")[1];
-    var slides = document.getElementById(slideshowId).getElementsByClassName("content-zone")[0].getElementsByClassName("slideshow-item"); 
+    var slides = $("#" + slideshowId + " .content-zone .slideshow-item");
 
     var next;
     for (var slideIndex=0;slideIndex<slides.length;slideIndex++)
         if (activated[slideshowId][slideIndex]) {
             next=getNextSlideIndex(slides.length, slideIndex);break; 
-        }  
-    if (next == slides.length-1) allSlidesWereSeen=true;
+        }
     ActivateSlide(slideshowId, slides, next, "next-slide"); 
     DeactivateSlide(slideshowId, slides, getPrevSlideIndex(slides.length, next));   
 }
@@ -95,31 +105,52 @@ function ShowNextSlide()
 
 function ActivateArrows(slideshowId)
 { 
-    leftarrow =  slideshowId + "-left";rightarrow = slideshowId + "-right";
-    document.getElementById(slideshowId).getElementsByClassName("left-arrow-area")[0].getElementsByTagName("div")[0].id = leftarrow;
-    document.getElementById(slideshowId).getElementsByClassName("right-arrow-area")[0].getElementsByTagName("div")[0].id = rightarrow;
-    document.getElementById(leftarrow).addEventListener("click", ShowPreviousSlide, false);
-    document.getElementById(rightarrow).addEventListener("click", ShowNextSlide, false);
+    leftarrowId =  slideshowId + "-left";
+    rightarrowId = slideshowId + "-right";
+    $("#" + slideshowId + " .left-arrow-area div").get(0).id = leftarrowId;
+    $("#" + slideshowId + " .right-arrow-area div").get(0).id = rightarrowId;
+    $("#" + leftarrowId).on("click", ShowPreviousSlide);
+    $("#" + rightarrowId).on("click", ShowNextSlide);
+}
+
+
+function NavigationClicked(navElementId, slideList) {
+    var splitted = navElementId.split("-");
+    var slideshowId = splitted[0] + "-" + splitted[1];
+    var slideIndex = parseInt(splitted[3]);
+    var lastIndex;
+    for (var i=0;i<slideList.length;i++) {
+        if (activated[slideshowId][i]==true) {
+            lastIndex=i;
+            break;
+        }
+    }
+    DeactivateSlide(slideshowId, slideList, lastIndex);
+    var newAnimation;
+    if (lastIndex < slideIndex) newAnimation = "next-slide";
+    else newAnimation = "prev-slide";
+    ActivateSlide(slideshowId, slideList, slideIndex, newAnimation);
 }
 
 
 function InitSlideshows()
 {
     for (var slideshowIndex=0;slideshowIndex<slideshows.length;slideshowIndex++) {
-    
-        slideshows[slideshowIndex].id = "slideshow-" + slideshowIndex.toString();
+        var slideshowId = "slideshow-" + slideshowIndex.toString();
+        slideshows[slideshowIndex].id = slideshowId;
         slides = slideshows[slideshowIndex].getElementsByClassName("content-zone")[0].getElementsByClassName("slideshow-item");
-        slideshows[slideshowIndex].getElementsByTagName("nav")[0].id = slideshows[slideshowIndex].id + "-navigation";
+        slideshows[slideshowIndex].getElementsByTagName("nav")[0].id = slideshowId + "-navigation";
         var progressbar = slideshows[slideshowIndex].getElementsByClassName("slideshow-progressbar")[0];
         slideshows[slideshowIndex].getElementsByClassName("slideshow-percentage")[0].innerHTML="0%";
         var navigationbar = slideshows[slideshowIndex].getElementsByTagName("nav")[0];
-        progressbar.id = slideshows[slideshowIndex].id + "-progressbar"; 
-        activated[slideshows[slideshowIndex].id] = {};
-        checked[slideshows[slideshowIndex].id] = 0;
+        progressbar.id = slideshowId + "-progressbar";
+        activated[slideshowId] = {};
+        checked[slideshowId] = {};
         
-        var barElement, navElement; 
+        var barElement, navElement;
         for (var slideIndex=0;slideIndex<slides.length;slideIndex++) {
-            slides[slideIndex].id = slideshows[slideshowIndex].id + "-slide-" + slideIndex.toString();   
+            checked[slideshowId][slideIndex] = false;
+            slides[slideIndex].id = slideshowId + "-slide-" + slideIndex.toString();
 
             barElement = document.createElement("div");  
             barElement.id = slides[slideIndex].id + "-progressbar";
@@ -128,22 +159,19 @@ function InitSlideshows()
             navElement.id = slides[slideIndex].id + "-navigationbar";
 
             progressbar.appendChild(barElement);
+            navElement.addEventListener("click", function(e) {NavigationClicked(this.id, slides);}, false);
             navigationbar.appendChild(navElement);
-            document.getElementById(navElement.id).addEventListener("click", ()=> {
-                console.log("click on ", navElement.id);
-                ActivateSlide(slideshows[slideshowIndex].id, slides, slideIndex, "none");
-            });
 
-            DeactivateSlide(slideshows[slideshowIndex].id, slides, slideIndex); 
-        } 
+            DeactivateSlide(slideshowId, slides, slideIndex);
+        }
     
         var prevslide, nextslide;
         if (slides.length > 0) { 
             if (slides.length > 1) {
-                ActivateArrows(slideshows[slideshowIndex].id);
-                ActivateSlide(slideshows[slideshowIndex].id, slides, 0, "next-slide");
+                ActivateArrows(slideshowId);
+                ActivateSlide(slideshowId, slides, 0, "next-slide");
             }
-            else ActivateSlide(slideshows[slideshowIndex].id, slides, 0, "next-slide", false);
+            else ActivateSlide(slideshowId, slides, 0, "next-slide", false);
         }
     }
 }
