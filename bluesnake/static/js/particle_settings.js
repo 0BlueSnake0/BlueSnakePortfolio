733af3126731eb29398ca086e7c38f1f2499ecef
Konstantin <sulwirld@gmail.com>
2022-06-30 22:41:55 -0400

    Create adaptive design.

--- bluesnake/static/js/particle_settings.js
+++ bluesnake/static/js/particle_settings.js
@@ -2,7 +2,13 @@ var leftParticleDurationBorder=3, rightParticleDurationBorder=15;
 var leftSizeBorder=4, rightSizeBorder=12;
 
 var particleAnimations = [
+    "particleMoveLeft", "particleMoveLeft", "particleMoveLeft", "particleMoveLeft",
+    "particleMoveRight", "particleMoveRight", "particleMoveRight", "particleMoveRight",
     "particleMoveTop", "particleMoveTop", "particleMoveTop", "particleMoveTop",
+    "particleMoveBottom", "particleMoveBottom", "particleMoveBottom", "particleMoveBottom",
+
     "particleMoveLeftTop","particleMoveLeftTop","particleMoveLeftTop","particleMoveLeftTop",
     "particleMoveRightTop","particleMoveRightTop","particleMoveRightTop","particleMoveRightTop",
-]; 
+    "particleMoveLeftBottom","particleMoveLeftBottom","particleMoveLeftBottom","particleMoveLeftBottom",
+    "particleMoveRightBottom","particleMoveRightBottom","particleMoveRightBottom","particleMoveRightBottom",
+];
