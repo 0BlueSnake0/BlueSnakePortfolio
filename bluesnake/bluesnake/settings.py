d1b30d81398b711ef037ff87a0f13d6b2259a5c1
Konstantin <70055801+0BlueSnake0@users.noreply.github.com>
2022-07-15 10:54:45 +0300

    Update settings.py

--- bluesnake/bluesnake/settings.py
+++ bluesnake/bluesnake/settings.py
@@ -6,11 +6,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
  
 SECRET_KEY = config('SECRET_KEY') 
   
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    'bluesnakeengineer.ru',
-    '127.0.0.1'
+    'bluesnakeengineer.ru' 
 ]
 
 
